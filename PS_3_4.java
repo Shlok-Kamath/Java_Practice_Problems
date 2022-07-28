e5731a9db6e873bbc9e4d2fbeb9aabdef31a545e
Shlok-Kamath <91421028+Shlok-Kamath@users.noreply.github.com>
2022-07-28 23:15:57 +0530

    Create PS_3_4.java

---
+++ PS_3_4.java
@@ -0,0 +1,16 @@
+public class Practice_Problems {
+
+    public static void main(String[] args) {
+
+        String statement = "Here is a double   space";
+        String statement2 = "Here is a   triple space";
+      
+      
+      // It will print 1st occurance of double space
+        System.out.println(statement.indexOf("  "));
+      
+      //It will print 1st occurance of triple space
+        System.out.println(statement2.indexOf("   "));
+
+    }
+}
