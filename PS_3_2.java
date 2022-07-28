2214b614dbb427479138901777f647467b784748
Shlok-Kamath <91421028+Shlok-Kamath@users.noreply.github.com>
2022-07-28 23:01:30 +0530

    Create PS_3_2.java

---
+++ PS_3_2.java
@@ -0,0 +1,18 @@
+public class Practice_Problems {
+
+    public static void main(String[] args) {
+
+        String original = "Shlok is dumb";
+      
+      //Use replace function to change spaces to underscore
+        String modified = original.replace(" ","_");
+      
+        
+      //The original string
+        System.out.println("The original string "+original);
+      
+      //The modified string
+        System.out.println("The modified string "+modified);
+
+    }
+}
