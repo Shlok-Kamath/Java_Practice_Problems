2274415052edb85c56a676c333968a8a124d54df
Shlok-Kamath <91421028+Shlok-Kamath@users.noreply.github.com>
2022-07-28 22:56:40 +0530

    Create PS_3_1.java

---
+++ PS_3_1.java
@@ -0,0 +1,19 @@
+public class Practice_Problems {
+
+    public static void main(String[] args) {
+
+        String name = "Shlok";
+      
+      //Use inbuilt function to change to lower case
+        String Lower = name.toLowerCase();
+        
+      
+      //The original String
+        System.out.println("The original name "+name);
+      
+      
+      //The modified String
+        System.out.println("Name changed to lower case "+Lower);
+
+    }
+}
