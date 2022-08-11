bc2ee562b982ffd317182ca2272c05746fabcf18
Shlok-Kamath <91421028+Shlok-Kamath@users.noreply.github.com>
2022-08-11 19:47:42 +0530

    Create PS_5_3.java

---
+++ PS_5_3.java
@@ -0,0 +1,13 @@
+ public class Practice_Problems {
+
+    public static void main(String[] args) {
+        int n;
+        Scanner scan = new Scanner(System.in);
+        n = scan.nextInt();
+
+
+        for (int i = 1; i<=10;i++){
+        System.out.println(n+" * "+i+" = "+n*i); // Used String Concatenation 
+    }
+    }
+}
