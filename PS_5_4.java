b3c9a9961e8949d9c9c4f9f3cf916d37c0afffe2
Shlok-Kamath <91421028+Shlok-Kamath@users.noreply.github.com>
2022-08-11 19:48:40 +0530

    Create PS_5_4.java

---
+++ PS_5_4.java
@@ -0,0 +1,11 @@
+ public class Practice_Problems {
+
+    public static void main(String[] args) {
+        int n=10;
+        while(n!=0){
+
+            System.out.println("10 * "+n+" = "+n*10);
+            n--;
+        }
+    }
+}
