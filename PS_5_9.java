3d33a1ab84290d72187a0d1a8e4fb47c05e1cb4a
Shlok-Kamath <91421028+Shlok-Kamath@users.noreply.github.com>
2022-08-11 19:55:33 +0530

    Create PS_5_9.java

---
+++ PS_5_9.java
@@ -0,0 +1,13 @@
+public class Practice_Problems {
+
+    public static void main(String[] args) {
+
+        int total = 0;
+        for (int i = 1; i<11 ; i++){
+
+            total += 8*i;
+        }
+
+        System.out.println("Total is: "+total);
+    }
+}
